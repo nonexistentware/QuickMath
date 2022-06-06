80029310f3088c9a41e23d9ce1f99ee541a51b44
nonexistentware <nonexistentware@gmail.com>
2022-06-06 22:31:28 +0300

    second commit

---
+++ app/src/main/java/com/nonexistentware/quickmath/Activity/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.nonexistentware.quickmath.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nonexistentware.quickmath.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/nonexistentware/quickmath/Activity/SelectGameActivity.java
+++ app/src/main/java/com/nonexistentware/quickmath/Activity/SelectGameActivity.java
@@ -3,14 +3,26 @@ package com.nonexistentware.quickmath.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.nonexistentware.quickmath.R;
 
 public class SelectGameActivity extends AppCompatActivity {
 
+
+
+    TextView aboutAppBtn, classicModeBtn, timeAttackModeBtn, scoreCounterTxt, winCounterTxt, levelCounterTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_game);
+
+
+        //to activity
+        classicModeBtn = findViewById(R.id.dashboard_to_classic_game_mode);
+        timeAttackModeBtn = findViewById(R.id.dashboard_to_time_attack_mode);
+        aboutAppBtn = findViewById(R.id.dashboard_about_app);
+
     }
 }
