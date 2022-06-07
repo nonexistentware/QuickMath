a451d96565c9db699eb12e66887e3e039a3e0870
nonexistentware <nonexistentware@gmail.com>
2022-06-07 21:53:45 +0300

    third commit

--- app/src/main/java/com/nonexistentware/quickmath/Activity/SelectGameActivity.java
+++ app/src/main/java/com/nonexistentware/quickmath/Activity/SelectGameActivity.java
@@ -2,16 +2,21 @@ package com.nonexistentware.quickmath.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.nonexistentware.quickmath.R;
 
 public class SelectGameActivity extends AppCompatActivity {
 
+    TextView aboutAppBtn, classicModeBtn, timeAttackModeBtn, scoreCounterTxt, winCounterTxt, levelCounterTxt, signOut;
 
-
-    TextView aboutAppBtn, classicModeBtn, timeAttackModeBtn, scoreCounterTxt, winCounterTxt, levelCounterTxt;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,21 @@ public class SelectGameActivity extends AppCompatActivity {
         classicModeBtn = findViewById(R.id.dashboard_to_classic_game_mode);
         timeAttackModeBtn = findViewById(R.id.dashboard_to_time_attack_mode);
         aboutAppBtn = findViewById(R.id.dashboard_about_app);
+
+        //btn
+        signOut = findViewById(R.id.dashboard_sign_ot_btn);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
 
     }
 }
