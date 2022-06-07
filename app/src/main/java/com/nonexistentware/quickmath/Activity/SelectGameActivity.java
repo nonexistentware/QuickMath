acd412b1a11b18c0561e82fc368ef0f7edf1677d
nonexistentware <nonexistentware@gmail.com>
2022-06-07 22:10:21 +0300

    update 1

--- app/src/main/java/com/nonexistentware/quickmath/Activity/SelectGameActivity.java
+++ app/src/main/java/com/nonexistentware/quickmath/Activity/SelectGameActivity.java
@@ -3,26 +3,40 @@ package com.nonexistentware.quickmath.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.nonexistentware.quickmath.R;
+import com.squareup.picasso.Picasso;
 
 public class SelectGameActivity extends AppCompatActivity {
 
-    TextView aboutAppBtn, classicModeBtn, timeAttackModeBtn, scoreCounterTxt, winCounterTxt, levelCounterTxt, signOut;
+    TextView aboutAppBtn, classicModeBtn, timeAttackModeBtn, scoreCounterTxt, duelWinCounterTxt, levelCounterTxt, playerGoogleName, signOut;
+    ImageView playerIconImg;
 
     private FirebaseAuth auth;
     private FirebaseUser firebaseUser;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_game);
 
+        //img
+        playerIconImg = findViewById(R.id.dashboard_user_profile_img);
+
+        //data to fetch
+        playerGoogleName = findViewById(R.id.dashboard_player_google_name);
+        scoreCounterTxt = findViewById(R.id.dashboard_score_counter);
+        levelCounterTxt = findViewById(R.id.dashboard_level_counter);
+        duelWinCounterTxt = findViewById(R.id.dashboard_duel_win_counter);
 
         //to activity
         classicModeBtn = findViewById(R.id.dashboard_to_classic_game_mode);
@@ -35,6 +49,32 @@ public class SelectGameActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         firebaseUser = auth.getCurrentUser();
 
+        classicModeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        timeAttackModeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        aboutAppBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+            }
+        });
+
+//        if (auth.getCurrentUser() != null) {
+//            reference = FirebaseDatabase.getInstance().getReference()
+//                    .child("Players").child(auth.getCurrentUser().getUid());
+//        }
+
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,5 +84,17 @@ public class SelectGameActivity extends AppCompatActivity {
             }
         });
 
+        loadPlayerData();
+
+    }
+
+    private void loadPlayerData() {
+        if (auth.getCurrentUser() != null) {
+            playerGoogleName.setText(firebaseUser.getDisplayName());
+            Picasso.get()
+                    .load(firebaseUser.getPhotoUrl())
+                    .into(playerIconImg);
+
+        }
     }
 }
